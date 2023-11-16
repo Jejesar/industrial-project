c1f58bfb5b19fe389c5664cff91c37ef284db21e
Jejesar <jejesar.pro@gmail.com>
2023-11-16 03:23:28 +0100

    Refactor monitoring page to display real-time data.

--- src/pages/monitoring.tsx
+++ src/pages/monitoring.tsx
@@ -3,28 +3,79 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import { Navbar, StatsCard } from "~/components";
 
-import variablesPLC from "~/assets/variables-api";
+// import tagsPLC from "~/assets/variables-api";
 
 const booleanToString = (bool: boolean) => {
   return bool ? "ON" : "OFF";
 };
 
+const formatString = (value: string | number | boolean) => {
+  if (typeof value === "boolean") return booleanToString(value);
+  if (typeof value === "string" && (value === "0" || value === "1"))
+    return booleanToString(value === "1");
+};
+
+interface Tag {
+  name: string;
+  value: string | number | boolean;
+  show: boolean;
+  date: string;
+}
+
+const tagsName = {
+  dc1: "DC1",
+  dp1: "DP1",
+  dc2: "DC2",
+};
+
 export default function Configuration({}) {
-  const [variables, setVariables] = useState({});
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [update, setUpdate] = useState(false);
 
   const getVariables = async () => {
-    const res = await fetch("/api/tags/get/last/all", {
+    const res = await fetch("/api/tags/get/all", {
       method: "POST",
     });
     const data = await res.json();
-    console.log(data);
 
-    // setVariables(data);
+    if (data.length === 0) return;
+
+    var equalsArray = true;
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].value !== tags[i]?.value || data[i].show !== tags[i]?.show) {
+        equalsArray = false;
+        break;
+      }
+    }
+
+    if (equalsArray === false) {
+      setTags(data);
+    }
   };
 
   useEffect(() => {
+    const timer = setInterval(() => {
+      setUpdate(!update);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [update]);
+
+  useEffect(() => {
     getVariables();
-  }, []);
+  }, [update]);
+
+  const showTags = (tags: Tag[]) => {
+    return tags.map((tag: Tag) => {
+      if (tag.show === false) return null;
+      return (
+        <StatsCard
+          key={tag.name}
+          title={tagsName[tag.name as keyof typeof tagsName] || tag.name}
+          stat={formatString(tag.value) || "N/A"}
+        />
+      );
+    });
+  };
 
   return (
     <>
@@ -49,22 +100,16 @@ export default function Configuration({}) {
           pt={5}
           px={{ base: 2, sm: 12, md: 17 }}
         >
-          <Grid templateColumns="repeat(3, 1fr)" gap={6} pb={10}>
-            {variablesPLC.map((v) => {
-              return (
-                <StatsCard
-                  key={v.name}
-                  title={v.longname ? v.longname : v.name}
-                  stat={booleanToString(
-                    variables[v.name as keyof typeof variables]
-                  )}
-                />
-              );
-            })}
-            {/* <StatsCard title={"DP1"} stat={booleanToString(variables.dc1)} />
-            <StatsCard title={"DC1"} stat={booleanToString(variables.dp1)} />
-            <StatsCard title={"DP2"} stat={booleanToString(variables.dc1)} />
-            <StatsCard title={"DC2"} stat={booleanToString(variables.dc1)} /> */}
+          <Grid
+            templateColumns={{
+              base: "repeat(2, 1fr)",
+              sm: "repeat(3, 1fr)",
+              md: "repeat(4, 1fr)",
+            }}
+            gap={6}
+            pb={10}
+          >
+            {showTags(tags)}
           </Grid>
         </Container>
       </main>
